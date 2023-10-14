4f3b78db7672cf0edc028f910698137877d92e4b
Preston Tom-Ying <preston.tomying@gmail.com>
2023-10-14 13:57:01 -0400

    fixed props

--- src/components/barGraph.js
+++ src/components/barGraph.js
@@ -6,7 +6,7 @@ export default function BarGraph(props) {
     // props contian category name
     return (
         <div class="barGraph">
-            <h2 class="graph-title">props.title</h2>
+            <h2 class="graph-title">{props.title}</h2>
             <Bar />
         </div>
     );
